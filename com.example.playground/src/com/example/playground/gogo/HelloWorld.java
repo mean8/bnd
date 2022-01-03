f2cb70039ca87d91ed2a2018af5682934590c386
DESKTOP-HOD36K5\user <dbkim@hncorp.world>
2022-01-03 14:51:13 +0900

    remove ms949 encoding

--- com.example.playground/src/com/example/playground/gogo/HelloWorld.java
+++ com.example.playground/src/com/example/playground/gogo/HelloWorld.java
@@ -3,7 +3,6 @@ package com.example.playground.gogo;
 import org.osgi.service.component.annotations.Component;
 import org.apache.felix.service.command.CommandProcessor;
 
-//커맨드가 hello일 경우 호출하고, 사용함수 = hello()
 @Component(property = {
         CommandProcessor.COMMAND_SCOPE + "=hello",
         CommandProcessor.COMMAND_FUNCTION + "=hello"},
